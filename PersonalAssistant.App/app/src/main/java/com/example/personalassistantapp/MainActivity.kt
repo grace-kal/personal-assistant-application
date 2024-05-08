d122fb17d20f810b5290496709d6d238154ea81d
grace-kal <kalinina.grace@gmail.com>
2024-05-09 01:13:06 +0300

    Added menu items, colors changed

--- PersonalAssistant.App/app/src/main/java/com/example/personalassistantapp/MainActivity.kt
+++ PersonalAssistant.App/app/src/main/java/com/example/personalassistantapp/MainActivity.kt
@@ -28,16 +28,26 @@ class MainActivity : AppCompatActivity() {
 
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null)
-                    .setAnchorView(R.id.fab).show()
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.nav_events,
+                R.id.nav_tasks,
+                R.id.nav_notes,
+                R.id.nav_schedules,
+                R.id.nav_wardrobe,
+                R.id.nav_news,
+                R.id.nav_contact_support
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
