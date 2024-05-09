6a69b5d8fe6377ef5188af8aa5b4cf0860e39301
grace-kal <kalinina.grace@gmail.com>
2024-05-09 04:06:04 +0300

    Adding user model

---
+++ PersonalAssistant.App/app/src/main/java/com/example/personalassistantapp/models/User.kt
@@ -0,0 +1,12 @@
+package com.example.personalassistantapp.models
+
+class User {
+    private val id = 0
+    private val username: String? = null
+    private val email: String? = null
+    private val password: String? = null
+    private val firstName: String? = null
+    private val lastName: String? = null
+    private val country: String? = null
+    private val city: String? = null
+}
