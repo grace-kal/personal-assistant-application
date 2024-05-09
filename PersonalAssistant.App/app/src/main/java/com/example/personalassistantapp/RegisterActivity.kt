33ef8e9c10607637ca473615f30b4adb6e84e12b
grace-kal <kalinina.grace@gmail.com>
2024-05-09 17:52:18 +0300

    Register activity countries take, android manifest login initial inflate

--- PersonalAssistant.App/app/src/main/java/com/example/personalassistantapp/RegisterActivity.kt
+++ PersonalAssistant.App/app/src/main/java/com/example/personalassistantapp/RegisterActivity.kt
@@ -6,6 +6,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
+
+
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,17 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val countryCodes = Locale.getISOCountries()
+
+        for (countryCode: String? in countryCodes) {
+            val obj = Locale("", countryCode)
+
+            println(
+                "Country Code = " + obj.country
+                        + ", Country Name = " + obj.displayCountry
+            )
         }
     }
 
