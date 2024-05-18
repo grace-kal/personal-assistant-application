e7e9af0896e3c483e8d88b3b39e83a139a704a8b
grace-kal <kalinina.grace@gmail.com>
2024-05-19 01:23:17 +0300

    Weather Controller

---
+++ PersonalAssistant.Api/PersonalAssistant.Api/Controllers/WeatherController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PersonalAssistant.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+        [HttpGet("Cities")]
+        public async Task<ActionResult<bool>> GetCities([FromQuery] string countryCode)
+        {
+            if (!countryCode.IsNullOrEmpty())
+                return true;
+            else return false;
+        }
+    }
+}
