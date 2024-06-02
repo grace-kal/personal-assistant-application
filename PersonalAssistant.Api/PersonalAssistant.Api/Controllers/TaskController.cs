81135493bc04363a1a8728111edaa788af1db29d
grace-kal <kalinina.grace@gmail.com>
2024-06-02 23:58:53 +0300

    datetime helper, task controller

---
+++ PersonalAssistant.Api/PersonalAssistant.Api/Controllers/TaskController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalAssistant.Services.Interfaces;
+
+namespace PersonalAssistant.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController(ITaskService service) : Controller
+    {
+        [HttpGet("GetAllTasksForDate")]
+        public async Task<ActionResult> GetAllEventsForDate([FromQuery] string email, string date)
+        {
+            //var dateNOTImplConv = DateTime.Now;
+            //return Ok(await service.GetAllEventsForDate(dateNOTImplConv, email));
+            return Ok();
+        }
+    }
+}
---
+++ PersonalAssistant.Api/PersonalAssistant.Api/Helpers/DateTimeHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace PersonalAssistant.Api.Helpers
+{
+    public class DateTimeHelper
+    {
+        public DateTime GetDateFromJsonString(string date)
+        {
+            const string format = "yyyy-MM-dd";
+
+            return DateTime.ParseExact(date, format, System.Globalization.CultureInfo.InvariantCulture);
+        }
+    }
+}
